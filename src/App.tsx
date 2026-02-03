1a692a9b8dcba9e4136e113621b7bfa245ef0dfe
digitalwebconnection <service@digitalwebconnection.com>
2026-02-03 14:58:14 +0530

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import './App.css'
 import Footer from './Component/Footer'
 import Header from './Component/Header'
 import HomeMain from './Component/HomePage/HomeMain'
+import WhatsAppWidget from './Component/WhatsAppWidget'
 
 function App() {
 
@@ -12,6 +13,7 @@ function App() {
       <Header/>
       <HomeMain/>
       <Footer/>
+      <WhatsAppWidget/>
     </>
   )
 }
